56eeb094f14c4464f2bfc023c334765174e65088
LC1A_20_shinkafukumi <k020g1205@g.neec.ac.jp>
2022-09-06 21:01:21 +0900

    Add Car Class

---
+++ DXlib1/Car.cpp
@@ -0,0 +1,26 @@
+#include "Car.h"
+#include "DxLib.h"
+Car::Car()
+{
+}
+
+Car::~Car()
+{
+}
+
+void Car::Init()
+{
+}
+
+void Car::Update()
+{
+}
+
+void Car::Finalize()
+{
+}
+
+void Car::Draw()
+{
+
+}
---
+++ DXlib1/Car.h
@@ -0,0 +1,14 @@
+#pragma once
+#include "Vector3.h"
+class Car
+{
+public:
+	Car();
+	~Car();
+	void Init();
+	void Update();
+	void Finalize();
+	void Draw();
+private:
+};
+
